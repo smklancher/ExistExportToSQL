210959df41760b439db08520f026620dad4c9c22
Stephen Klancher <smknight@gmail.com>
2019-12-26 19:28:07 -0800

    test

---
+++ ExistExportToSQL/Publish.cmd
@@ -0,0 +1,13 @@
+cd /d %~dp0
+
+dotnet publish -r win-x64 -c Release -p:PublishSingleFile=true -p:PublishTrimmed=true -o:.\Publish\SelfContained\win
+dotnet publish -r win-x64 -c Release -p:PublishSingleFile=true --no-self-contained -o:.\Publish\FrameworkDependentSingleFile\win
+
+dotnet publish -r linux-x64 -c Release -p:PublishSingleFile=true -p:PublishTrimmed=true -o:.\Publish\SelfContained\linux
+dotnet publish -r linux-x64 -c Release -p:PublishSingleFile=true --no-self-contained -o:.\Publish\FrameworkDependentSingleFile\linux
+
+dotnet publish -r osx-x64 -c Release -p:PublishSingleFile=true -p:PublishTrimmed=true -o:.\Publish\SelfContained\osx
+dotnet publish -r osx-x64 -c Release -p:PublishSingleFile=true --no-self-contained -o:.\Publish\FrameworkDependentSingleFile\osx
+
+dotnet publish -c Release -o:.\CrossPlatform
+pause
